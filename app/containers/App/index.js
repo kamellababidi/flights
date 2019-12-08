9f06a7a77d96a8a79fef8cf0d714996f047e75e2
kamel allababidi <kamel@repocoin.io>
2019-12-08 11:00:52 +0400

    init project

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -32,18 +32,18 @@ export default function App() {
   return (
     <AppWrapper>
       <Helmet
-        titleTemplate="%s - React.js Boilerplate"
-        defaultTitle="React.js Boilerplate"
+        titleTemplate="%s - flights"
+        defaultTitle="flights"
       >
         <meta name="description" content="A React.js Boilerplate application" />
       </Helmet>
-      <Header />
+      {/* <Header /> */}
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/features" component={FeaturePage} />
         <Route path="" component={NotFoundPage} />
       </Switch>
-      <Footer />
+      {/* <Footer /> */}
       <GlobalStyle />
     </AppWrapper>
   );
